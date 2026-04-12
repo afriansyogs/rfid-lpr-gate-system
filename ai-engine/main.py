de42c1fdf465044e67db5374d8a2bdfb46f3d3a9
ayt <afriansyoga@gmail.com>
2026-04-12 08:10:02 +0700

    Fitur: RFID Tap Out with verification plat number ai

--- ai-engine/main.py
+++ ai-engine/main.py
@@ -2,3 +2,14 @@ from fastapi import FastAPI
 from scanner import capture_and_scan
 
 app = FastAPI(title="LPR AI Engine")
+
+@app.get("/api/ai/scan")
+def scan_static_plate():
+    plat_nomor = capture_and_scan()
+
+    print(f"Plat nomor terdeteksi: {plat_nomor}")
+    
+    return {
+        "status": "success",
+        "plate_number": plat_nomor
+    }
--- ai-engine/scanner.py
+++ ai-engine/scanner.py
@@ -1,3 +1,26 @@
 import cv2
 import easyocr
+import os
+import re
 from ultralytics import YOLO
+
+print("Loading AI Model...")
+reader = easyocr.Reader(['en'])
+print("AI Model Ready!")
+
+def capture_and_scan():
+    image_path = "dummy_plates/plat.jpg"
+
+    img = cv2.imread(image_path)
+    result = reader.readtext(img)
+
+    raw_text = "".join([text for (bbox, text, prob) in result])
+    clean_text = "".join(e for e in raw_text if e.isalnum()).upper()
+    
+    # regex plat indo 
+    match = re.search(r'([A-Z]{1,2}\d{1,4}[A-Z]{1,3})', clean_text)
+
+    if match:
+        valid_plate = match.group(1)
+        return valid_plate
+    return "UNKNOWN"
